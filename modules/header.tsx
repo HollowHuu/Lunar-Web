d24d34afb8a7413fbcdd21cef50f194574b86ef3
Nicolas Wulf Kähler <63862081+HollowHuu@users.noreply.github.com>
2023-10-16 14:59:52 +0200

    Catching up

---
+++ modules/header.tsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+
+    return (
+        <div>
+            <img src="https://cdn.discordapp.com/attachments/774650675593609268/1136331735265722408/Bot_logo_witout_back.png" alt="logo" width="40" />
+        </div>
+    )
+}
